ac82f40c4ba8d48f263f57bf4c6fdc8779cba51e
Nikhil Anand <anand.nik@gmail.com>
2015-12-07 21:57:26 +0000

    update 10

--- CODE10/GeneticAlgorithm.py
+++ CODE10/GeneticAlgorithm.py
@@ -8,13 +8,13 @@ from time import gmtime, strftime
 
 
 
-
+'''
 "As per Code 9-10 details"
 candidates=100
 generations=1000
 mutationRate=0.05
 lives=5
-
+'''
 
 "Run Baseline"
 def baseline(model,decisions=10,objectives=2,num=10000):
@@ -27,9 +27,7 @@ def baseline(model,decisions=10,objectives=2,num=10000):
     for i in range(objectives) :
         list1=[]
         for each in setOfCandidates:
-            ####$print each
-            ####$print len(each.getObjectives())
-            #list1.append(each.getObjectives()[i])
+            
             list1.append(each.fx[i])
 
         mini.append(numpy.min(list1))
@@ -42,7 +40,6 @@ def baseline(model,decisions=10,objectives=2,num=10000):
 def crossover(child,Parent1,Parent2):
     while True:
         x=random.randint(0,len(Parent1.x))
-        ####$print "cut chromosome at index = ",x
         X = list(numpy.array(Parent1.x)[:x])
         Y = list(numpy.array(Parent2.x)[x:])
         child.x= X + Y
@@ -58,7 +55,6 @@ def lt(x,y): return x < y
 #type 1
 def binaryDomination(model1, model2):
     bettered = False
-    #for i,(xi,yi) in enumerate(zip(model1.getObjectives(),model2.getObjectives())):
     for i,(xi,yi) in enumerate(zip(model1.fx,model2.fx)):
 
         if lt(xi,yi):
@@ -80,12 +76,12 @@ def updateParetoFrontier(bestPF,newPF,lives):
                 if cand1 not in tmpCandList:
                     tmpCandList.append(cand1)
                     bestPF.remove(cand2)
-
+                    
     if tmpCandList:
         bestPF.extend(tmpCandList)
-        return lives
-    else:
-        return lives
+    return lives
+
+
         
         
 def type2(list1, list2):
@@ -97,7 +93,6 @@ def type2(list1, list2):
         
 "True if samples dominated by the front"
 def Dominates(pfPoint,someObjectives):
-    #pfObjectives=pfPoint.getObjectives()
     pfObjectives=pfPoint.fx
     if pfObjectives==someObjectives:
         return False
@@ -135,7 +130,6 @@ def hypervolume(paretoFront,min,max,sample=10000):
     count=0.0    
     m=paretoFront[0].objectives
 
-    ###$print "calculating hypervolume......"
     for i in xrange(sample):
         someObjectives = []
         for i in xrange(m):
@@ -151,19 +145,10 @@ def hypervolume(paretoFront,min,max,sample=10000):
 def GeneticAlgorithm(model,decisions=4,objectives=2,someSeed=30,candidates=100,generations=1000,mutationRate=0.05,lives=5):
     
     random.seed(someSeed)
-    #########$$$$$$$print "\nstart Baseline:",strftime("%Y-%m-%d %H:%M:%S", gmtime())
     min,max=baseline(model,decisions=decisions,objectives=objectives,num=10000)
-    #########$$$$$$$print "end Baseline:",strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-    
-    ###$print "#####model : ",model.__name__
-    
-    ###$print "Start creating list of candidates"
 
     listOfCandidates=[model(objectives,decisions) for _ in xrange(candidates)]
-    
-    ###$print "Length of candidate list:",len(listOfCandidates)
-    #########$$$$$$$print "\nstart finding first best:",strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
     PF=[]
     for cand1 in listOfCandidates:
@@ -175,35 +160,24 @@ def GeneticAlgorithm(model,decisions=4,objectives=2,someSeed=30,candidates=100,g
         if flag:
             PF.append(cand1)
     bestPF=PF[:]
-    #########$$$$$$$print "end :",strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    #########$$$$$$$print "$$$$$$$$$$$$Best PF points:",len(bestPF)
-
     
     life=0
-    ###$print "\ngenerations...",
     for i in xrange(generations):
-        ###$print i,",",
         newCandidateList=[]
         for j in xrange(candidates):
             child=model(objectives,decisions)
             choose=numpy.random.choice(len(PF),2,replace=True)
             Parent1=PF[choose[0]]
             Parent2=PF[choose[1]]
-            #print "\nstart crossover:",strftime("%Y-%m-%d %H:%M:%S", gmtime())
             child = crossover(child,Parent1,Parent2) #"Genetically Crossover parents"
-            ##########$$$$print "End:",strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-            if random.random()<mutationRate:#  *(2**life):
-                #########$$$$$$$print "\nstart mmutate:",strftime("%Y-%m-%d %H:%M:%S", gmtime())
+            if random.random()<mutationRate:#
                 child.setDecisions() #Genetically mutate child
-                #########$$$$$$$print "Endnd:",strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
             newCandidateList.append(child)
             
         newPF=[]
         
-        
-        #########$$$$$$$print "\nstart finding new pf:",strftime("%Y-%m-%d %H:%M:%S", gmtime())
         for cand1 in listOfCandidates:
             flag=True
             for cand2 in listOfCandidates:
@@ -212,35 +186,15 @@ def GeneticAlgorithm(model,decisions=4,objectives=2,someSeed=30,candidates=100,g
                     break
             if flag:
                 newPF.append(cand1)
-                
-        #########$$$$print "End:",strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-        #########$$$$$$$print "\nstart updating:",strftime("%Y-%m-%d %H:%M:%S", gmtime())
-
-        updated=updateParetoFrontier(bestPF,newPF,lives)
-        #########$$$$$$$print "$$$$$$$$$$$$Best updates PF points:",len(bestPF)
-
-        if updated == 0:
-            #########$$$$$$$print "lives = ",lives," terminating early"
+        lives=updateParetoFrontier(bestPF,newPF,lives)
+    
+        if lives == 0:
             lives = 5
-            #########$$$$print "End:",strftime("%Y-%m-%d %H:%M:%S", gmtime())
             break
-        #########$$$$print "End:",strftime("%Y-%m-%d %H:%M:%S", gmtime())
-
-        '''
-        if updated:
-            life=0
-        else:
-            life=life+1
-        if life==lives:
-            break
-        '''
 
         listOfCandidates=newCandidateList
         pf=newPF
-    ###$print ""
-    #########$$$$$$$print "Start HVcaluculations:",strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    HyperVolume = hypervolume(bestPF,min,max,10000)
-    #########$$$$$$$print "End:",strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
+    HyperVolume = hypervolume(bestPF,min,max,10000)
     return bestPF,HyperVolume
